ade9e487f0bc3326e29a2bd7df83476a3a2a35ab
delindell <daviselindell@gmail.com>
2020-02-26 23:55:30 -0600

    setup stuff completed

---
+++ javascripts/components/bearForm.js
@@ -0,0 +1,49 @@
+import util from "../helpers/util.js"
+
+const bearFormDomStringBuilder = () => {
+    let domString = ''
+    domString += '<form id="full-bear-form">'
+    domString += '<div class="form-group">'
+    domString += '<input type="text" class="form-control" id="bear-name" aria-describedby="bearName" placeholder="Enter Bear Name">'
+    domString += '</div>'
+    domString += '<div class="form-group">'
+    domString += '<input type="text" class="form-control" id="bear-image" placeholder="Bear Image Url">'
+    domString += '</div>'
+    domString += '<button type="submit" class="btn btn-primary" id="bear-submit-button">Submit</button>'
+    domString += '</form>'
+    util.printToDom('bear-form', domString)
+    formEvent()
+}
+
+const formEvent = () => {
+    document.getElementById('bear-submit-button').addEventListener('click', submitForm)
+}
+
+const bears = [
+    {
+        id: 'bear1',
+        name: 'Fred',
+        imgUrl: 'https://images.unsplash.com/photo-1530595467537-0b5996c41f2d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80'
+    }
+
+]
+
+console.log(bears)
+
+const getBears = () => {
+    return bears
+}
+
+const submitForm = (e) => {
+    e.preventDefault();
+    const brandNewBear = {
+        id: `bear${bears.length + 1}`,
+        name: document.getElementById("bear-name").value,
+        imgUrl: document.getElementById('bear-image').value
+    }
+    bears.push(brandNewBear)
+    document.getElementById('full-bear-form').reset();
+    console.log(bears) 
+}
+
+export default { bearFormDomStringBuilder, getBears }
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,9 @@
-console.log('what up world.')
+import formBuilder from "./components/bearForm.js"
+import bearBuilder from "./components/river.js"
+
+const init = () => {
+    formBuilder.bearFormDomStringBuilder();
+    bearBuilder.bearDomStringBuilder();
+}
+
+init();
